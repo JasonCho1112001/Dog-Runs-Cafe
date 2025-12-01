5af45274c26c5dc3e0f27627b188f979829fb0dd
ngonza81 <ngonza81@ucsc.edu>
2025-11-30 17:48:36 -0800

    Detects whether or cup if spill occurs + Added spawner on tray after halfway point

--- Dog Runs Cafe/Assets/Scripts/BallGroundCheck.cs
+++ Dog Runs Cafe/Assets/Scripts/BallGroundCheck.cs
@@ -10,10 +10,17 @@ public class BallGroundCheck : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision)
+{
+    if (collision.collider.CompareTag("Ground"))
     {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            timer.Lose();
-        }
+        Debug.Log("A liquid ball hit the ground!");
+        timer.Lose();
     }
+
+    if (collision.collider.CompareTag("Tray"))
+    {
+        Debug.Log("A liquid ball touched the tray!");
+        timer.Lose();
+    }
+}
 }
